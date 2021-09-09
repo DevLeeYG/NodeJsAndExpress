ac36791b42969c6593563557e50f5187254cb0b0
hi33741 <hi33741@naver.com>
2021-09-09 21:13:12 +0900

    handlebars

---
+++ meadowlark.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.engine(
+  "handlebars",
+  expressHandlebars({
+    defaultLayout: "main",
+  })
+);
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => res.render("home"));
+
+app.get("/about", (req, res) => res.render("about"));
+
+// custom 404 page
+app.use((req, res) => {
+  res.status(404);
+  res.render("404");
+});
+
+// custom 500 page
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  res.status(500);
+  res.render("500");
+});
+
+app.listen(port, () =>
+  console.log(
+    `Express started on http://localhost:${port}; ` +
+      `press Ctrl-C to terminate.`
+  )
+);
